7c961bd6226a679b4c54ec053ce1c1a13add982d
Minjae <minjaelee@comcom.ai>
2023-06-20 23:55:10 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TiktokApp());
+}
+
+class TiktokApp extends StatelessWidget {
+  const TiktokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tiktok',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Container(),
+    );
+  }
+}
