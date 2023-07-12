528314be93feba78aa1c1aeeddc01b74d5705e99
Minjae <minjaelee@comcom.ai>
2023-07-12 21:48:15 +0700

    Post Video Button

---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/sizes.dart';
+
+class PostVideoButton extends StatelessWidget {
+  const PostVideoButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Positioned(
+          right: Sizes.size20,
+          child: Container(
+            height: Sizes.size32,
+            width: Sizes.size24,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: const Color(0xff61D4F0),
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: Sizes.size20,
+          child: Container(
+            height: Sizes.size32,
+            width: Sizes.size24,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size8,
+            ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(
+                Sizes.size8,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: Sizes.size32,
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size12,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size6,
+            ),
+          ),
+          child: const Center(
+            child: FaIcon(
+              FontAwesomeIcons.plus,
+              color: Colors.black,
+              size: Sizes.size16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
