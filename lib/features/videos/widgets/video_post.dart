2f43e2cc311da6119f05eb1affc8ce56ad42bead
Minjae <minjaelee@comcom.ai>
2023-07-16 15:11:33 +0700

    showModalBottomSheet

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -4,6 +4,7 @@ import 'package:marquee/marquee.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/videos/widgets/video_button.dart';
+import 'package:tiktok/features/videos/widgets/video_comments.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -90,7 +91,9 @@ class _VideoPostState extends State<VideoPost>
 
   void _onVisibilityChanged(VisibilityInfo info) {
     print('Video: #${widget.index} is ${info.visibleFraction * 100}%');
-    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+    if (info.visibleFraction == 1 &&
+        !_videoPlayerController.value.isPlaying &&
+        !_isPaused) {
       _videoPlayerController.play();
     }
   }
@@ -112,6 +115,18 @@ class _VideoPostState extends State<VideoPost>
     setState(() {
       _isMoreTagsShowed = !_isMoreTagsShowed;
     });
+  }
+
+  void _onCommentsTap(BuildContext context) async {
+    if (_videoPlayerController.value.isPlaying) {
+      _onTogglePause();
+    }
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) => const VideoComments(),
+      backgroundColor: Colors.transparent,
+    );
+    _onTogglePause();
   }
 
   @override
@@ -259,12 +274,12 @@ class _VideoPostState extends State<VideoPost>
               ],
             ),
           ),
-          const Positioned(
+          Positioned(
             bottom: Sizes.size20,
             right: Sizes.size10,
             child: Column(
               children: [
-                CircleAvatar(
+                const CircleAvatar(
                   radius: Sizes.size24,
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
@@ -274,17 +289,20 @@ class _VideoPostState extends State<VideoPost>
                   child: Text('MJ'),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.solidHeart,
                   text: '2.9M',
                 ),
                 Gaps.v24,
-                VideoButton(
-                  icon: FontAwesomeIcons.solidComment,
-                  text: '33K',
+                GestureDetector(
+                  onTap: () => _onCommentsTap(context),
+                  child: const VideoButton(
+                    icon: FontAwesomeIcons.solidComment,
+                    text: '33K',
+                  ),
                 ),
                 Gaps.v24,
-                VideoButton(
+                const VideoButton(
                   icon: FontAwesomeIcons.share,
                   text: 'Share',
                 ),
