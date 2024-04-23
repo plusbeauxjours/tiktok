06dcf41c6f4d1b40d4b4d541da21f04ace94115c
plusbeauxjours <plusbeauxjours@gmail.com>
2024-04-23 10:22:01 +0700

    AnimatedList

--- lib/features/inbox/screens/chats_screen.dart
+++ lib/features/inbox/screens/chats_screen.dart
@@ -11,6 +11,22 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 500,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,44 +35,52 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text('Direct messages'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(foregroundImage),
-              child: Text('plusbeauxjours'),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "hannah",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(foregroundImage),
+                  child: Text('plusbeauxjours'),
                 ),
-                Text(
-                  "5:12 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      "hannah",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "5:12 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text("Let's having fun things!"),
+              ),
             ),
-            subtitle: const Text("Let's having fun things!"),
-          )
-        ],
+          );
+        },
       ),
     );
   }
