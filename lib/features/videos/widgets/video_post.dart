ac99ce03cb3e5b5635c86850645799783721a2f7
plusbeauxjours <plusbeauxjours@gmail.com>
2024-05-04 23:06:44 +0700

    kIsWeb

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
@@ -43,6 +44,9 @@ class _VideoPostState extends State<VideoPost>
     _videoPlayerController = VideoPlayerController.asset(widget.video);
     await _videoPlayerController.initialize();
     await _videoPlayerController.setLooping(true);
+    if (kIsWeb) {
+      await _videoPlayerController.setVolume(0);
+    }
     _videoPlayerController.addListener(_onVideoChange);
     if (mounted) setState(() {});
   }
