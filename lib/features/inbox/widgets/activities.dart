6f11ace53f0004ebc147d92fc7f4e37fbc2837b7
plusbeauxjours <plusbeauxjours@gmail.com>
2024-05-18 21:57:30 +0700

    Dark Mode

--- lib/features/inbox/widgets/activities.dart
+++ lib/features/inbox/widgets/activities.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/rawData/inboxs.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/utils/common_utils.dart';
 
 class Activities extends StatelessWidget {
   final void Function(String) onDismissed;
@@ -22,6 +23,7 @@ class Activities extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = isDarkMode(context);
     return Stack(
       children: [
         ListView(
@@ -78,30 +80,31 @@ class Activities extends StatelessWidget {
                     width: Sizes.size52,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      color: Colors.white,
+                      color: isDark ? Colors.grey.shade800 : Colors.white,
                       border: Border.all(
-                        color: Colors.grey.shade400,
+                        color: isDark
+                            ? Colors.grey.shade800
+                            : Colors.grey.shade400,
                         width: Sizes.size1,
                       ),
                     ),
                     child: const Center(
                       child: FaIcon(
                         FontAwesomeIcons.bell,
-                        color: Colors.black,
                       ),
                     ),
                   ),
                   title: RichText(
                     text: TextSpan(
                       text: "Account updates:",
-                      style: const TextStyle(
+                      style: TextStyle(
                         fontWeight: FontWeight.w600,
-                        color: Colors.black,
                         fontSize: Sizes.size16,
+                        color: isDark ? null : Colors.black,
                       ),
                       children: [
                         const TextSpan(
-                          text: " Upload longer videos",
+                          text: "Upload longer videos",
                           style: TextStyle(
                             fontWeight: FontWeight.normal,
                           ),
@@ -133,9 +136,9 @@ class Activities extends StatelessWidget {
         SlideTransition(
           position: panelAnimation,
           child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
+            decoration: BoxDecoration(
+              color: Theme.of(context).appBarTheme.backgroundColor,
+              borderRadius: const BorderRadius.only(
                 bottomLeft: Radius.circular(
                   Sizes.size5,
                 ),
@@ -151,9 +154,8 @@ class Activities extends StatelessWidget {
                   ListTile(
                     title: Row(
                       children: [
-                        FaIcon(
+                        Icon(
                           tab["icon"],
-                          color: Colors.black,
                           size: Sizes.size16,
                         ),
                         Gaps.h20,
--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,14 @@ class TiktokApp extends StatelessWidget {
           unselectedLabelColor: Colors.grey.shade500,
           indicatorColor: Colors.black,
         ),
+        listTileTheme: const ListTileThemeData(
+          iconColor: Colors.black,
+        ),
       ),
       darkTheme: ThemeData(
-        tabBarTheme: const TabBarTheme(indicatorColor: Colors.white),
+        tabBarTheme: const TabBarTheme(
+          indicatorColor: Colors.white,
+        ),
         textSelectionTheme: const TextSelectionThemeData(
           cursorColor: Color(0xFFE9435A),
         ),
