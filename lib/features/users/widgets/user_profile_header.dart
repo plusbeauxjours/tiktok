9224025552f04e4a2b8e0de2bac65a56c358c00d
plusbeauxjours <plusbeauxjours@gmail.com>
2024-07-04 17:33:39 +0700

    userProfileHeader refactorying

--- lib/features/users/widgets/user_profile_header.dart
+++ lib/features/users/widgets/user_profile_header.dart
@@ -5,18 +5,24 @@ import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/users/widgets/avatar.dart';
 
 class UserProfileHeader extends StatelessWidget {
+  static const double _fontSize = Sizes.size18;
+  static const double _iconSize = Sizes.size16;
+  static const double _dividerThickness = Sizes.size1;
+  static const double _dividerWidth = Sizes.size32;
+  static const double _dividerIndent = Sizes.size14;
+
   final String name;
   final String username;
   final bool hasAvatar;
   final String uid;
 
   const UserProfileHeader({
-    super.key,
+    Key? key,
     required this.name,
     required this.username,
     required this.hasAvatar,
     required this.uid,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,102 +34,74 @@ class UserProfileHeader extends StatelessWidget {
           uid: uid,
         ),
         Gaps.v20,
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              username != "undefined" ? '@$username' : '@$name',
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size18,
-              ),
-            ),
-            Gaps.h5,
-            FaIcon(
-              FontAwesomeIcons.solidCircleCheck,
-              color: Colors.blue.shade500,
-              size: Sizes.size16,
-            ),
-          ],
-        ),
+        _buildUsernameRow(),
         Gaps.v24,
         SizedBox(
           height: Sizes.size52 + 1,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Column(
-                children: [
-                  const Text(
-                    '37',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.v3,
-                  Text(
-                    'Following',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ],
-              ),
-              VerticalDivider(
-                color: Colors.grey.shade400,
-                thickness: Sizes.size1,
-                width: Sizes.size32,
-                indent: Sizes.size14,
-                endIndent: Sizes.size14,
-              ),
-              Column(
-                children: [
-                  const Text(
-                    '10.5M',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.v3,
-                  Text(
-                    'Followers',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ],
-              ),
-              VerticalDivider(
-                color: Colors.grey.shade400,
-                thickness: Sizes.size1,
-                width: Sizes.size32,
-                indent: Sizes.size14,
-                endIndent: Sizes.size14,
-              ),
-              Column(
-                children: [
-                  const Text(
-                    '149.3M',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.v3,
-                  Text(
-                    'Likes',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ],
-              ),
+              _buildStatColumn('37', 'Following'),
+              _buildVerticalDivider(),
+              _buildStatColumn('10.5M', 'Followers'),
+              _buildVerticalDivider(),
+              _buildStatColumn('149.3M', 'Likes'),
             ],
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildUsernameRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          username != "undefined" ? '@$username' : '@$name',
+          style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: _fontSize,
+          ),
+        ),
+        Gaps.h5,
+        FaIcon(
+          FontAwesomeIcons.solidCircleCheck,
+          color: Colors.blue.shade500,
+          size: _iconSize,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildStatColumn(String count, String text) {
+    return Column(
+      children: [
+        Text(
+          count,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: _fontSize,
+          ),
+        ),
+        Gaps.v3,
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.grey.shade500,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildVerticalDivider() {
+    return VerticalDivider(
+      color: Colors.grey.shade400,
+      thickness: _dividerThickness,
+      width: _dividerWidth,
+      indent: _dividerIndent,
+      endIndent: _dividerIndent,
     );
   }
 }
