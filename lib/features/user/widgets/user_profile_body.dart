69aa01d166bda15c4b3bce4835e085eb657fd0cd
plusbeauxjours <plusbeauxjours@gmail.com>
2024-08-26 20:16:44 +0700

    set: refactorying UserProfileBody

--- lib/features/user/widgets/user_profile_body.dart
+++ lib/features/user/widgets/user_profile_body.dart
@@ -23,216 +23,174 @@ class UserProfileBody extends ConsumerStatefulWidget {
 }
 
 class _UserProfileBodyState extends ConsumerState<UserProfileBody> {
-  final TextEditingController _bioController = TextEditingController();
-  final TextEditingController _linkController = TextEditingController();
+  late final TextEditingController _bioController;
+  late final TextEditingController _linkController;
 
   bool _isBioEdit = false;
   bool _isLinkEdit = false;
 
-  String _bio = '';
-  String _link = '';
-
-  void _toggleBioEdit() {
-    _isBioEdit = !_isBioEdit;
-    ref.read(editProvider.notifier).toggleEditMode();
-    if (!_isBioEdit && widget.bio != _bio) {
-      ref.read(userProvider.notifier).onUpdateUserBio(_bio);
-    }
-    setState(() {});
-  }
-
-  void _toggleLinkEdit() {
-    _isLinkEdit = !_isLinkEdit;
-    if (!_isLinkEdit && widget.link != _link) {
-      ref.read(userProvider.notifier).onUpdateUserLink(_link);
-    }
-    setState(() {});
-  }
-
   @override
   void initState() {
     super.initState();
-    _bioController.text = widget.bio;
-    _linkController.text = widget.link;
+    _bioController = TextEditingController(text: widget.bio);
+    _linkController = TextEditingController(text: widget.link);
 
-    _bioController.addListener(() {
-      _bio = _bioController.text;
-      setState(() {});
+    _bioController.addListener(() => setState(() {}));
+    _linkController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _bioController.dispose();
+    _linkController.dispose();
+    super.dispose();
+  }
+
+  void _toggleEdit(bool isEdit, String field) {
+    setState(() {
+      if (field == 'bio') {
+        _isBioEdit = !_isBioEdit;
+        ref.read(editProvider.notifier).toggleEditMode();
+        if (!_isBioEdit && widget.bio != _bioController.text) {
+          ref.read(userProvider.notifier).onUpdateUserBio(_bioController.text);
+        }
+      } else {
+        _isLinkEdit = !_isLinkEdit;
+        if (!_isLinkEdit && widget.link != _linkController.text) {
+          ref
+              .read(userProvider.notifier)
+              .onUpdateUserLink(_linkController.text);
+        }
+      }
     });
-    _linkController.addListener(() {
-      _link = _linkController.text;
-      setState(() {});
-    });
+  }
+
+  Widget _buildEditableField(
+    bool isEdit,
+    TextEditingController controller,
+    String hintText,
+    String field,
+  ) {
+    return isEdit
+        ? CstTextField(
+            controller: controller,
+            hintText: hintText,
+            suffix:
+                _buildEditButtons(controller, () => _toggleEdit(isEdit, field)),
+          )
+        : _buildDisplayField(controller.text, field);
+  }
+
+  Widget _buildEditButtons(
+      TextEditingController controller, VoidCallback onToggle) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: controller.clear,
+            child: FaIcon(
+              FontAwesomeIcons.solidCircleXmark,
+              color: Colors.grey.shade500,
+              size: Sizes.size20,
+            ),
+          ),
+          Gaps.h5,
+          GestureDetector(
+            onTap: onToggle,
+            child: FaIcon(
+              FontAwesomeIcons.solidCircleCheck,
+              color: Colors.grey.shade500,
+              size: Sizes.size20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDisplayField(String text, String field) {
+    final isEditMode = ref.watch(editProvider)['isEditMode'];
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: kIsWeb ? null : MediaQuery.of(context).size.width - 100,
+          child: field == 'link'
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (text.isNotEmpty)
+                      const FaIcon(
+                        FontAwesomeIcons.link,
+                        size: Sizes.size12,
+                      ),
+                    Text(
+                      ' $text',
+                      style: const TextStyle(fontWeight: FontWeight.w600),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                    ),
+                  ],
+                )
+              : Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                ),
+        ),
+        Gaps.h8,
+        if (isEditMode)
+          GestureDetector(
+            onTap: () =>
+                _toggleEdit(field == 'bio' ? _isBioEdit : _isLinkEdit, field),
+            child: const FaIcon(
+              FontAwesomeIcons.pen,
+              size: Sizes.size14,
+            ),
+          ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final isEditMode = ref.watch(editProvider)['isEditMode'];
     return SizedBox(
       width: 450,
       child: Column(
         children: [
           FractionallySizedBox(
             widthFactor: 0.66,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  fit: FlexFit.tight,
-                  flex: 4,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: Sizes.size14,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(Sizes.size4),
-                    ),
-                    child: const Text(
-                      'Follow',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: Sizes.size14),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(Sizes.size4),
+              ),
+              child: const Text(
+                'Follow',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
                 ),
-              ],
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
           Gaps.v14,
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
-            child: _isBioEdit
-                ? CstTextField(
-                    controller: _bioController,
-                    hintText: 'Please write your intro',
-                    suffix: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          GestureDetector(
-                            onTap: () => _bioController.clear(),
-                            child: FaIcon(
-                              FontAwesomeIcons.solidCircleXmark,
-                              color: Colors.grey.shade500,
-                              size: Sizes.size20,
-                            ),
-                          ),
-                          Gaps.h5,
-                          GestureDetector(
-                            onTap: _toggleBioEdit,
-                            child: FaIcon(
-                              FontAwesomeIcons.solidCircleCheck,
-                              color: Colors.grey.shade500,
-                              size: Sizes.size20,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                : Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: kIsWeb
-                            ? null
-                            : MediaQuery.of(context).size.width - 100,
-                        child: Text(
-                          // 'All highlights and where to watch live matches on FIFA+',
-                          widget.bio,
-                          textAlign: TextAlign.center,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 3,
-                        ),
-                      ),
-                      Gaps.h8,
-                      if (isEditMode)
-                        GestureDetector(
-                          onTap: _toggleBioEdit,
-                          child: const FaIcon(
-                            FontAwesomeIcons.pen,
-                            size: 14,
-                          ),
-                        ),
-                    ],
-                  ),
+            child: _buildEditableField(
+                _isBioEdit, _bioController, 'Please write your intro', 'bio'),
           ),
           Gaps.v14,
-          _isLinkEdit
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
-                  child: CstTextField(
-                    controller: _linkController,
-                    hintText: 'Please write your link',
-                    suffix: Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          GestureDetector(
-                            onTap: () => _linkController.clear(),
-                            child: FaIcon(
-                              FontAwesomeIcons.solidCircleXmark,
-                              color: Colors.grey.shade500,
-                              size: Sizes.size20,
-                            ),
-                          ),
-                          Gaps.h5,
-                          GestureDetector(
-                            onTap: _toggleLinkEdit,
-                            child: FaIcon(
-                              FontAwesomeIcons.solidCircleCheck,
-                              color: Colors.grey.shade500,
-                              size: Sizes.size20,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                )
-              : Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: kIsWeb
-                          ? null
-                          : MediaQuery.of(context).size.width - 100,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (widget.link.isNotEmpty)
-                            const FaIcon(
-                              FontAwesomeIcons.link,
-                              size: Sizes.size12,
-                            ),
-                          Text(
-                            ' ${widget.link}',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Gaps.h8,
-                    if (isEditMode)
-                      GestureDetector(
-                        onTap: _toggleLinkEdit,
-                        child: const FaIcon(
-                          FontAwesomeIcons.pen,
-                          size: Sizes.size14,
-                        ),
-                      ),
-                  ],
-                ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
+            child: _buildEditableField(
+                _isLinkEdit, _linkController, 'Please write your link', 'link'),
+          ),
         ],
       ),
     );
